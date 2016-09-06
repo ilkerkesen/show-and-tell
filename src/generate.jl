f6f78a0c07288ddb435fe7015ec0b4f635858e99
Ilker Kesen <ilkerksn@gmail.com>
2016-09-06 22:27:42 +0300

    add testing capabilities to generate script

--- src/generate.jl
+++ src/generate.jl
@@ -13,40 +13,50 @@ function main(args)
         ("--datafile"; help="data file contains dataset splits and vocabulary")
         ("--loadfile"; help="pretrained model file")
         ("--savefile"; help="save generated captions")
+        ("--datasplit"; default="tst"; help="data split is going to be used")
         ("--maxlen"; arg_type=Int; default=20; help="max sentence length")
+        ("--testing"; action=:store_true)
+        ("--amount"; arg_type=Int; default=20; help="generation amount in case of testing")
     end
+
+    println("Datetime: ", now()); flush(STDOUT)
 
     # parse args
     isa(args, AbstractString) && (args=split(args))
     o = parse_args(args, s; as_symbols=true); println(o); flush(STDOUT)
 
     # load data
-    tst = load(o[:datafile], "tst")
+    tst = load(o[:datafile], o[:datasplit])
     voc = load(o[:datafile], "voc")
     net = load(o[:loadfile], "net")
-    println("Data loaded...")
-    flush(STDOUT)
+    @printf("Data loaded [%s]\n", now()); flush(STDOUT)
 
     filenames = Any[]
     captions = Any[]
+    counter, ti = 0, now()
     for i = 1:length(tst)
-        fn, cap = generate(net, tst[i], voc, o[:maxlen])
+        o[:testing] && counter > o[:amount] && break
+        fn, orig, gen = generate(net, tst[i], voc, o[:maxlen])
+
         if !(fn in filenames)
             push!(filenames, fn)
-            push!(captions, cap)
-            println(fn, ": ", cap)
-            flush(STDOUT)
+            push!(captions, gen)
+            @printf("filename: %s\noriginal: %s\ngenerated: %s\n\n",
+                    fn, orig, gen); flush(STDOUT)
+            counter += 1
         end
     end
 
+    tf = now()
+    @printf("\nTime elapsed: %s [%s]\n", tf-ti, tf)
+
     # FIXME: make data convenient for perl script
-    save(o[:savefile], "filenames", filenames)
-    save(o[:savefile], "captions", captions)
+    !testing && save(o[:savefile], "filenames", filenames, "captions", captions)
 end
 
 function generate(f, sample, voc, maxlen)
     reset!(f)
-    fn, vis, _ = sample
+    fn, vis, vec = sample
     vis = reshape(vis, length(vis), 1)
     forw(f, vis; decoding=false)
     word = SOS
@@ -66,7 +76,9 @@ function generate(f, sample, voc, maxlen)
         pop!(sentence)
     end
 
-    return (fn, join(sentence[2:end], " "))
+    orig = join(map(i -> index2word(voc,i), vec[2:end-1]), " ")
+    gen = join(sentence[2:end], " ")
+    return (fn, orig, gen)
 end
 
 !isinteractive() && !isdefined(Core.Main, :load_only) && main(ARGS)
