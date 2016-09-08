343feaa06b5dd4786e69c541f64a1e0e391c8142
Ilker Kesen <ilkerksn@gmail.com>
2016-09-09 00:41:01 +0300

    revisit generation script

--- src/generate.jl
+++ src/generate.jl
@@ -1,3 +1,7 @@
+using GPUChecker
+using CUDArt
+CUDArt.device(first_min_used_gpu())
+
 using Knet
 using ArgParse
 using JLD
@@ -10,12 +14,14 @@ function main(args)
     s.description = "Caption generation script for the model."
 
     @add_arg_table s begin
-        ("--datafile"; help="data file contains dataset splits and vocabulary")
-        ("--loadfile"; help="pretrained model file")
-        ("--savefile"; help="save generated captions")
+        ("--datafile"; help="data file contains vocabulary")
+        ("--modelfile"; help="trained model file")
+        ("--savedir"; help="save generations and references")
         ("--datasplit"; default="tst"; help="data split is going to be used")
         ("--maxlen"; arg_type=Int; default=20; help="max sentence length")
         ("--testing"; action=:store_true)
+        ("--shuffle"; action=:store_true)
+        ("--debug"; action=:store_true)
         ("--amount"; arg_type=Int; default=20; help="generation amount in case of testing")
     end
 
@@ -25,33 +31,64 @@ function main(args)
     isa(args, AbstractString) && (args=split(args))
     o = parse_args(args, s; as_symbols=true); println(o); flush(STDOUT)
 
+    # checkdir
+    isdir(o[:savedir]) && (println("savedir does not exist."); flush(STDOUT); quit())
+
     # load data
-    tst = load(o[:datafile], o[:datasplit])
+    tst = load(o[:jsonfile], o[:datasplit])
     voc = load(o[:datafile], "voc")
-    net = load(o[:loadfile], "net")
+    net = load(o[:modelfile], "net")
     @printf("Data loaded [%s]\n", now()); flush(STDOUT)
 
-    filenames = Any[]
-    captions = Any[]
+    o[:testing] && o[:shuffle] && shuffle!(tst)
+
+    refs, gens = Dict(), Dict()
     counter, ti = 0, now()
+
+    # generate captions
     for i = 1:length(tst)
         o[:testing] && counter > o[:amount] && break
-        fn, orig, gen = generate(net, tst[i], voc, o[:maxlen])
+        fn, gen = generate(net, tst[i], voc, o[:maxlen])
+        orig = vec2sen(voc, tst[i][3])
 
-        if !(fn in filenames)
-            push!(filenames, fn)
-            push!(captions, gen)
-            @printf("filename: %s\noriginal: %s\ngenerated: %s\n\n",
-                    fn, orig, gen); flush(STDOUT)
-            counter += 1
+        !haskey(gens, fn) && (gens[fn] = gen; refs[fn] = Any[])
+        push!(refs[fn], orig]
+
+        if o[:debug]
+            @printf("filename: %s\noriginal: %s\ngenerated: %s [%s]\n\n",
+                    fn, orig, gen, now()); flush(STDOUT)
         end
     end
 
+    # some validation
+    ks = sort(collect(keys(refs)))
+    sz = ks[1]
+    for i = 2:length(ks)
+        ks[i] == sz || (println("Validation error!"); quit())
+    end
+
+    # open file streams
+    files = Any[]
+    for i = 1:sz
+        push!(files, open(abspath(joinpath(o[:savedir], "refs$(i).txt")), "w"))
+    end
+    resfile = open(abspath(joinpath(o[:savedir], "results.txt")), "w")
+
+    for i = 1:length(keys)
+        write(resfile, string(gens[keys[i]], "\n"))
+        for j = 1:sz
+            write(files[j], string(refs[keys[i]][j], "\n"))
+        end
+    end
+
+    # close file streams
+    for i = 1:sz
+        close(files[i])
+    end
+    close(resfile)
+
     tf = now()
     @printf("\nTime elapsed: %s [%s]\n", tf-ti, tf)
-
-    # FIXME: make data convenient for perl script
-    !testing && save(o[:savefile], "filenames", filenames, "captions", captions)
 end
 
 function generate(f, sample, voc, maxlen)
@@ -76,9 +113,9 @@ function generate(f, sample, voc, maxlen)
         pop!(sentence)
     end
 
-    orig = join(map(i -> index2word(voc,i), vec[2:end-1]), " ")
-    gen = join(sentence[2:end], " ")
-    return (fn, orig, gen)
+    return (fn, join(sentence[2:end], " "))
 end
+
+vec2sen(voc::Vocabulary, sen) = join(map(i -> index2word(voc,i), vec[2:end-1]), " ")
 
 !isinteractive() && !isdefined(Core.Main, :load_only) && main(ARGS)
